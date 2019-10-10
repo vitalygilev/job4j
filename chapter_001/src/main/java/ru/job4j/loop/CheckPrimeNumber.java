2f8cd47b221248a29c5c0c30e387b1c9b91c1fc5
vitalygilev <vitvit1@yandex.ru>
2019-10-10 12:19:30 +0300

    Простое число.[#186735]

---
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+
+    public boolean check(int finish) {
+        boolean prime = false;
+        for (int i = 2; i < finish; i++) {
+            prime = (finish % i == 0);
+            if (prime) break;
+        }
+        return !prime;
+    }
+}
