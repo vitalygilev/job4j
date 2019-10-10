857ff0a939e644a26ed21bdea5568a529ae44377
vitalygilev <vitvit1@yandex.ru>
2019-10-10 16:18:48 +0300

    JavaDoc. Code convention[#186730]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,27 +1,58 @@
 package ru.job4j.calculator;
 
+/**
+ * Класс для вычисления арифметических функций.
+ *
+ * @author Vitaly Gilev (vitvit1@yandex.ru)
+ * @version 1
+ * @since 10.10.2019
+ */
 public class Calculator {
 
+    /**
+     * Сложение
+     * @param first первое слагаемое
+     * @param second  второе слагаемое
+     */
     public static void add(double first, double second) {
         double result =  first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
 
+    /**
+     * Деление
+     * @param first делимое
+     * @param second  делитель
+     */
     public static void div(double first, double second) {
         double result =  first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
 
+    /**
+     * Умножение
+     * @param first первый сомножитель
+     * @param second  второй сомножитель
+     */
     public static void multiply(double first, double second) {
         double result =  first * second;
         System.out.println(first + "*" + second + " = " + result);
     }
 
+    /**
+     * Вычитание
+     * @param first уменьшаемое
+     * @param second  вычитаемое
+     */
     public static void subtract(double first, double second) {
         double result =  first - second;
         System.out.println(first + "-" + second + " = " + result);
     }
 
+    /**
+     * Запуск калькулятора
+     * @param args параметры
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * Package for calculate task.
+ *
+ * @author Vitaly Gilev (vitvit1@yandex.ru)
+ * @version 1
+ * @since 10.10.2019
+*/
+package ru.job4j.calculator;
+
