4b85cb975e7bf72cc425817a9aa350307a3f7bd3
vitalygilev <vitvit1@yandex.ru>
2019-10-11 15:04:00 +0300

    Поиск минимального числа в массиве.[#186752]

--- chapter_001/src/main/java/ru/job4j/array/Min.java
+++ chapter_001/src/main/java/ru/job4j/array/Min.java
@@ -11,4 +11,5 @@ public class Min {
         return min;
     }
 
+
 }
--- chapter_001/src/test/java/ru/job4j/array/MinTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MinTest.java
@@ -16,6 +16,7 @@ public class MinTest {
         );
     }
 
+
     @Test
     public void whenLastMin() {
         assertThat(
