ef201f6e54a98eb1e6ecf9faba29f11d5e4077e8
vitalygilev <vitvit1@yandex.ru>
2019-10-13 08:45:33 +0300

    Выигрышные комбинации в сокобан[#186743]

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,59 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+
+    public static boolean isWin(char[][] board) {
+        boolean result;
+        int winRowXCounter = 0;
+        int winColumnXCounter = 0;
+        char sign;
+        for (int row = 0; row < board.length; row++) {
+            if (winRowXCounter != board.length) winRowXCounter = 0;
+            for (int cell = 0; cell < board.length; cell++) {
+                sign = board[row][cell];
+                System.out.print(sign);
+
+                if (sign == 'X'){
+                    winRowXCounter += 1;
+                    winColumnXCounter += cell;
+                }
+            }
+            System.out.println();
+        }
+        result = (winRowXCounter == board.length) || (winColumnXCounter % board.length == 0);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[][] hasWinVertical = {
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+        };
+        boolean win = isWin(hasWinVertical);
+        System.out.println("A board has a winner : " + win);
+        System.out.println();
+        char[][] hasWinHor = {
+                {'_', '_', '_', '_', '_'},
+                {'X', 'X', 'X', 'X', 'X'},
+                {'_', '_', '_', '_', '_'},
+                {'_', '_', '_', '_', '_'},
+                {'_', '_', '_', '_', '_'},
+        };
+        boolean winHor = isWin(hasWinHor);
+        System.out.println("A board has a winner : " + winHor);
+        System.out.println();
+        char[][] notWin = {
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', 'X', '_', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+                {'_', '_', 'X', '_', '_'},
+        };
+        boolean lose = isWin(notWin);
+        System.out.println("A board has a winner : " + lose);
+    }
+
+}
