7aa8050101735f795a1c00a09dfc56ab07510b96
vitalygilev <vitvit1@yandex.ru>
2019-10-23 13:04:30 +0300

    Поля объекта[#186868]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryEat(Ball ball) {
+        System.out.println("Very strange situation. Ball tries to eat himself!");
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/Fox.java
@@ -0,0 +1,7 @@
+package ru.job4j.oop;
+
+public class Fox {
+    public void tryEat(Ball ball) {
+        System.out.println("Candidate Fox! We have a winner (brazen red muzzle)!!");
+    }
+}
