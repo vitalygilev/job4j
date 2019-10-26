70e3f2a343a05cd0cd3357c292df766f1f004226
vitalygilev <vitvit1@yandex.ru>
2019-10-26 09:15:52 +0300

    Массивы и модели.[#186888]

---
+++ chapter_002/src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,27 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private String name;
+    private int pagesCount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPagesCount() {
+        return pagesCount;
+    }
+
+    public void setPagesCount(int pagesCount) {
+        this.pagesCount = pagesCount;
+    }
+
+    public Book(String name, int pagesCount) {
+        this.name = name;
+        this.pagesCount = pagesCount;
+    }
+}
