7756c6d2dc2474f3a30b7c4bf14071a6261294f1
vitalygilev <vitvit1@yandex.ru>
2019-10-27 13:22:25 +0300

    Приведение типов. Повышение типа и понижение типа.[#186847]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Freshman.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Freshman extends Student{
+
+    private String group;
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+}
