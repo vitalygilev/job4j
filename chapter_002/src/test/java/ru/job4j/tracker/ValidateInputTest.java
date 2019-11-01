48d680690b7ba09f5942ec9a8c56fd6940e18880
vitalygilev <vitvit1@yandex.ru>
2019-11-01 08:26:24 +0300

    Рефакторинг - Шаблон Декоратор для валидатора.[#186853] (Исправление)

--- chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -17,11 +17,27 @@ public class ValidateInputTest {
         ValidateInput input = new ValidateInput(
                 new StubInput(new String[] {"invalid", "1"})
         );
-        input.askInt("Enter", 1);
+        input.askInt("Enter", 2);
         assertThat(
                 new String(mem.toByteArray()),
                 is(String.format("Please enter validate data again.%n"))
         );
         System.setOut(out);
     }
+    @Test
+    public void whenNumberOutOfRange() {
+        ByteArrayOutputStream mem = new ByteArrayOutputStream();
+        PrintStream out = System.out;
+        System.setOut(new PrintStream(mem));
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"-1", "1"})
+        );
+        input.askInt("Enter", 2);
+        assertThat(
+                new String(mem.toByteArray()),
+                is(String.format("Please select key from menu %n"))
+        );
+        System.setOut(out);
+    }
+
 }
