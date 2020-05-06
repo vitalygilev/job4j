008ed20c0be64272a19a483ad756a3f7ef5c5c62
Vitaly Gilev <vitvit1@yandex.ru>
2020-05-06 18:43:41 +0300

    5.3.3.1 Очередь на двух стеках[#204630]

--- chapter_j_001/src/main/java/ru/job4j/collection/ForwardLinked.java
+++ chapter_j_001/src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -44,6 +44,10 @@ public class ForwardLinked<T> implements Iterable<T> {
         return result;
     }
 
+    public boolean isEmpty() {
+        return head == null;
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
