b70227c5cdb6caea651dcabe692367b98cc6089a
Vitaly Gilev <vitvit1@yandex.ru>
2020-05-10 11:33:07 +0300

    Создать модель User[#204634]

---
+++ chapter_j_001/src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.Calendar;
+
+public class User {
+
+    public String name;
+    public int children;
+    public Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
