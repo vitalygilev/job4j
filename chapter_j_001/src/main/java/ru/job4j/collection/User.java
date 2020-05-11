8eb2cb1163149b79bf329a13083ca92a995c6d6c
Vitaly Gilev <vitvit1@yandex.ru>
2020-05-11 13:11:28 +0300

    4. Переопределить только equals[#204637]

--- chapter_j_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_j_001/src/main/java/ru/job4j/collection/User.java
@@ -15,11 +15,26 @@ public class User {
         this.birthday = birthday;
     }
 
-    @Override
+    /*@Override
     public int hashCode() {
         int result = name.hashCode();
         result = 31 * result + children;
         result = 31 * result + birthday.hashCode();
         return result;
+    }*/
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+                && name.equals(user.name)
+                && birthday.equals(user.birthday);
     }
+
 }
