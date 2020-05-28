616cab3133eee9350db6b0e47943e80aad5b6110
v.gilev <vitvit1@yandex.ru>
2020-05-28 11:09:41 +0300

    Добавить метод boolean isBinary()[1712#204658] Исправление #1

--- chapter_j_001/src/main/java/ru/job4j/collection/Tree.java
+++ chapter_j_001/src/main/java/ru/job4j/collection/Tree.java
@@ -8,6 +8,14 @@ public class Tree<E> implements SimpleTree<E> {
 
     private final Node<E> root;
 
+    abstract class Checker {
+        private E value;
+        abstract boolean checkElement(Node<E> el);
+        public Checker(E value) {
+            this.value = value;
+        }
+    }
+
     Tree(final E root) {
         this.root = new Node<>(root);
     }
@@ -16,21 +24,21 @@ public class Tree<E> implements SimpleTree<E> {
     public boolean add(E parent, E child) {
         boolean rsl = false;
         Optional<Node<E>> parentNode = findBy(parent);
-        if (parentNode.isPresent() && !findBy(child).isPresent()) {
+        if (parentNode.isPresent() && findBy(child).isEmpty()) {
             rsl = true;
             parentNode.get().children.add(new Node<>(child));
         }
         return rsl;
     }
 
-    public boolean isBinary() {
-        boolean rsl = true;
+    public Optional<Node<E>> loop(Checker checker) {
+        Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.children.size() > 2) {
-                rsl = false;
+            if (checker.checkElement(el)) {
+                rsl = Optional.of(el);
                 break;
             }
             data.addAll(el.children);
@@ -38,20 +46,25 @@ public class Tree<E> implements SimpleTree<E> {
         return rsl;
     }
 
+    public boolean isBinary() {
+        Checker curChecker = new Checker(null) {
+            @Override
+            boolean checkElement(Node<E> el) {
+                return (el.children.size() > 2);
+            }
+        };
+        return loop(curChecker).isEmpty();
+    }
+
     @Override
     public Optional<Node<E>> findBy(E value) {
-        Optional<Node<E>> rsl = Optional.empty();
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.value.equals(value)) {
-                rsl = Optional.of(el);
-                break;
+        Checker curChecker = new Checker(value) {
+            @Override
+            boolean checkElement(Node<E> el) {
+                return (el.value.equals(value));
             }
-            data.addAll(el.children);
-        }
-        return rsl;
+        };
+        return loop(curChecker);
     }
 
 }
