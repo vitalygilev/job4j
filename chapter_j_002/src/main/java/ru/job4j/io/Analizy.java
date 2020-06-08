92bb3a07590483bf9f77b3e4b9c3dd63f389bc74
v.gilev <vitvit1@yandex.ru>
2020-06-08 08:52:26 +0300

    Анализ доступности сервера.[859#204664] Исправление #1

--- chapter_j_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_j_002/src/main/java/ru/job4j/io/Analizy.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.StringJoiner;
 import java.util.stream.Collectors;
@@ -21,39 +24,42 @@ public class Analizy {
         return out.toString().split(System.lineSeparator());
     }
 
-    public void unavailable(String source, String target) {
-        String[] sourceContent = loadSourceFile(source);
-        String startTime = "";
-        String endTime = "";
-        Set<String> errorTypes = Stream.of("400", "500").collect(Collectors.toSet());
+    private static void writeToFile(String target, List<String> content) {
         try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            for (String curSourceString : sourceContent) {
-                String[] recordStruct = curSourceString.split(" ");
-                if (errorTypes.contains(recordStruct[0])) {
-                    if (startTime.isEmpty()) {
-                        startTime = recordStruct[1];
-                    } else {
-                         endTime = recordStruct[1];
-                    }
-                } else {
-                    if (!startTime.isEmpty() && !endTime.isEmpty()) {
-                        out.println(startTime + ";" + endTime);
-                        startTime = "";
-                        endTime = "";
-                    }
-                }
+            for (String curString : content) {
+                out.println(curString);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void unavailable(String source, String target) {
+        String[] sourceContent = loadSourceFile(source);
+        ArrayList<String> targetContent = new ArrayList<>();
+        String startTime = "";
+        String endTime = "";
+        Set<String> errorTypes = Stream.of("400", "500").collect(Collectors.toSet());
+        for (String curSourceString : sourceContent) {
+            String[] recordStruct = curSourceString.split(" ");
+            if (errorTypes.contains(recordStruct[0])) {
+                if (startTime.isEmpty()) {
+                    startTime = recordStruct[1];
+                } else {
+                    endTime = recordStruct[1];
+                }
+            } else {
+                if (!startTime.isEmpty() && !endTime.isEmpty()) {
+                    targetContent.add(startTime + ";" + endTime);
+                    startTime = "";
+                    endTime = "";
+                }
+            }
         }
+        writeToFile(target, targetContent);
+    }
+
+    public static void main(String[] args) {
+        writeToFile("unavailable.csv", Arrays.asList("15:01:30;15:02:32", "15:10:30;23:12:32"));
     }
 }
