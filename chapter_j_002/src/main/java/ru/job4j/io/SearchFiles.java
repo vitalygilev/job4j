f3fd645ebbfa2ce911e3fcc4de4d2321cd8dd399
v.gilev <vitvit1@yandex.ru>
2020-06-09 12:42:35 +0300

    Сканирование файловой системы.[106929#204662]

---
+++ chapter_j_002/src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,53 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchFiles implements FileVisitor<Path> {
+
+    private List<Path> paths = new ArrayList<>();
+    private Comparator comparator;
+
+    public List<Path> getPaths() {
+        return paths;
+    }
+
+    public SearchFiles(Comparator comparator) {
+        this.comparator = comparator;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (comparator.compare(file)) {
+            paths.add(file);
+        }
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+
+}
+
+interface Comparator {
+    boolean compare(Path curPath);
+}
