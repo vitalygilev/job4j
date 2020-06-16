b636458fac5179e6ead4a24a3117ec3168260052
v.gilev <vitvit1@yandex.ru>
2020-06-16 08:48:09 +0300

    Бот[7921#204675]

--- chapter_j_002/src/main/java/ru/job4j/io/EchoServer.java
+++ chapter_j_002/src/main/java/ru/job4j/io/EchoServer.java
@@ -1,8 +1,12 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -14,11 +18,23 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
+                    String query = "";
                     while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
-                        bye = bye || str.contains("Bye");
+                        bye = bye || str.contains("Exit");
+                        if (str.contains("GET")) {
+                            query = str.substring(str.indexOf("=") + 1, str.indexOf(" HTTP/"));
+                        }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    if (!query.isEmpty()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        if ("Exit".equals(query)) {
+                            out.write("Завершить работу сервера.".getBytes(StandardCharsets.UTF_16));
+                        } else {
+                            out.write(query.getBytes(StandardCharsets.UTF_16));
+                        }
+
+                    }
                 }
             }
         }
