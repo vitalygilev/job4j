07a01c8f2c390ded3bfa8e799b30842353b851da
v.gilev <vitvit1@yandex.ru>
2020-07-24 16:50:27 +0300

    Фильтры[1732#204609] Исправление #1

--- goods_select.sql
+++ goods_select.sql
@@ -7,7 +7,13 @@
 --select * from products as p where p.name LIKE '%МОРОЖЕНОЕ%';
 
 --3. Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
---select * from products as p where EXTRACT(month from p.expired_date) = EXTRACT(month from now()) + 1;
+--select * from products as p 
+--	where 
+--		case when EXTRACT(month from now()) > 12 THEN
+--			EXTRACT(month from p.expired_date) = 1
+--		else
+--			EXTRACT(month from p.expired_date) = EXTRACT(month from now()) + 1
+--		end;
 
 --4. Написать запрос, который выводит самый дорогой продукт.
 --select * from products as p order by price desc limit 1;
@@ -20,10 +26,15 @@
 --6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
 --select * from products as p
 --join types as t on 
---p.type_id = t.id and (t.name = 'СЫР' or t.name = 'МОЛОКО');
+--p.type_id = t.id 
+--where t.name = 'СЫР' or t.name = 'МОЛОКО';
 
 --7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.  
---select * from products as p where p.balance < 10;
+--select t.name, SUM(p.balance) from products as p
+-- left join types as t
+-- on p.type_id = t.id
+-- group by (t.name)
+-- having sum(p.balance) < 10
 
 --8. Вывести все продукты и их тип.
 --select * from products as p
