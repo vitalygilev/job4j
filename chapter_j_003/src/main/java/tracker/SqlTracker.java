e97ed4c17469045dce1a5bd10138bc2cc64ffcbb
v.gilev <vitvit1@yandex.ru>
2020-08-10 09:03:41 +0300

    Трекер SQL[1734#204601] Исправление #1

--- chapter_j_003/src/main/java/tracker/SqlTracker.java
+++ chapter_j_003/src/main/java/tracker/SqlTracker.java
@@ -8,10 +8,11 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.Random;
 
 public class SqlTracker implements Store {
     private Connection cn;
+    private PreparedStatement pst;
+    private ResultSet rs;
     private static Logger log = LoggerFactory.getLogger(SqlTracker.class);
 
     public void init() {
@@ -30,11 +31,13 @@ public class SqlTracker implements Store {
         }
     }
 
-    private int executeUpdate(PreparedStatement st) {
+    private int executeUpdate(boolean closePst) {
         int result = -1;
         try {
-            result = st.executeUpdate();
-            st.close();
+            result = pst.executeUpdate();
+            if (closePst) {
+                pst.close();
+            }
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
@@ -42,15 +45,21 @@ public class SqlTracker implements Store {
     }
 
     private void initiateTasksTableExists() throws SQLException {
-        executeUpdate(
-                cn.prepareStatement("create table if not exists tasks ("
-                        + "id varchar(2000) primary key unique not null,"
-                        + "name varchar(2000) unique not null)")
-        );
+        pst = cn.prepareStatement(" create table if not exists tasks ("
+                + "id serial primary key,"
+                + "name varchar(2000) unique not null)");
+        executeUpdate(true);
+
     }
 
     @Override
     public void close() throws Exception {
+        if (rs != null) {
+            rs.close();
+        }
+        if (pst != null) {
+            pst.close();
+        }
         if (cn != null) {
             cn.close();
         }
@@ -58,46 +67,45 @@ public class SqlTracker implements Store {
 
     @Override
     public Item add(Item item) throws SQLException {
-        item.setId(this.generateId());
-        PreparedStatement pst = cn.prepareStatement("insert into tasks (id, name) values (? , ?)");
-        pst.setString(1, item.getId());
-        pst.setString(2, item.getName());
-        executeUpdate(pst);
+        pst = cn.prepareStatement("insert into tasks (name) values (?)", Statement.RETURN_GENERATED_KEYS);
+        pst.setString(1, item.getName());
+        if (executeUpdate(false) != -1) {
+            rs = pst.getGeneratedKeys();
+            if (rs.next()) {
+                item.setId(rs.getString("id"));
+            }
+            rs.close();
+            pst.close();
+        }
         return item;
-    }
-
-    private String generateId() {
-        Random rm = new Random();
-        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
     }
 
     @Override
     public boolean replace(String id, Item item) throws SQLException {
-        PreparedStatement pst = cn.prepareStatement("update tasks set name = ? where id = ?");
+        pst = cn.prepareStatement("update tasks set name = ? where id = ?");
         pst.setString(1, item.getName());
-        pst.setString(2, id);
-        int result = executeUpdate(pst);
+        pst.setInt(2, Integer.parseInt(id));
+        int result = executeUpdate(true);
         return result == 1;
     }
 
     @Override
     public boolean delete(String id) throws SQLException {
-        PreparedStatement pst = cn.prepareStatement("delete from tasks where id = ?");
-        pst.setString(1, id);
-        int result = executeUpdate(pst);
+        pst = cn.prepareStatement("delete from tasks where id = ?");
+        pst.setInt(1, Integer.parseInt(id));
+        int result = executeUpdate(true);
         return result == 1;
     }
 
-    private List<Item> executeQuery(PreparedStatement st) {
+    private List<Item> executeQuery() {
         List<Item> result = new ArrayList<>();
-        ResultSet rs;
         try {
-            rs = st.executeQuery();
+            rs = pst.executeQuery();
             while (rs.next()) {
-                result.add(new Item(rs.getString("id"), rs.getString("name")));
+                result.add(new Item(Integer.toString(rs.getInt("id")), rs.getString("name")));
             }
             rs.close();
-            st.close();
+            pst.close();
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
@@ -106,23 +114,23 @@ public class SqlTracker implements Store {
 
     @Override
     public List<Item> findAll() throws SQLException {
-        PreparedStatement pst = cn.prepareStatement("select tasks.id, tasks.name from tasks");
-        return executeQuery(pst);
+        pst = cn.prepareStatement("select tasks.id, tasks.name from tasks");
+        return executeQuery();
     }
 
     @Override
     public List<Item> findByName(String key) throws SQLException {
-        PreparedStatement pst = cn.prepareStatement("select tasks.id, tasks.name from tasks where name = ?");
+        pst = cn.prepareStatement("select tasks.id, tasks.name from tasks where name = ?");
         pst.setString(1, key);
-        return executeQuery(pst);
+        return executeQuery();
     }
 
     @Override
     public Item findById(String id) throws SQLException {
         Item result = null;
-        PreparedStatement pst = cn.prepareStatement("select tasks.id, tasks.name from tasks where id = ?");
-        pst.setString(1, id);
-        List<Item> resultQuery = executeQuery(pst);
+        pst = cn.prepareStatement("select tasks.id, tasks.name from tasks where id = ?");
+        pst.setInt(1, Integer.parseInt(id));
+        List<Item> resultQuery = executeQuery();
         if (resultQuery.size() > 0) {
             result = new Item(resultQuery.get(0).getId(), resultQuery.get(0).getName());
         }
