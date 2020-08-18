b942d430e60b1d824e6679dea68bf205e92f44c0
v.gilev <vitvit1@yandex.ru>
2020-08-18 09:52:26 +0300

    Quartz[175122#204619]

--- chapter_002/src/main/java/ru/job4j/oop/Machine.java
+++ chapter_002/src/main/java/ru/job4j/oop/Machine.java
@@ -3,12 +3,12 @@ package ru.job4j.oop;
 import java.util.Arrays;
 
 public class Machine {
-    private final int[] COINS = {10, 5, 2, 1};
+    private final int[] coins = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
-        for (int coin : COINS) {
+        for (int coin : coins) {
             while (money - price - coin >= 0) {
                 money -= coin;
                 rsl[size++] = coin;
